afe13580bddf7ec6b12fb13f9a097c008a23bb00
Tristan Vidal <trsgot@gmail.com>
2024-03-12 01:37:35 +0000

    Refactor getHello method to return JSON response

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getJsonHello(): string {
+    return JSON.stringify({ message: 'Hello World! 🧨' });
   }
 }
