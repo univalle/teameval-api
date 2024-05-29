8188f7c63294dd73edd67fe0152784beb56d0f1f
Tristan <tres@duck.com>
2024-05-29 03:41:34 +0000

    chore: Update npm dependencies and add Swagger documentation for admins controller

--- src/admins/admins.controller.ts
+++ src/admins/admins.controller.ts
@@ -1,20 +1,24 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { AdminsService } from './admins.service';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('admins')
 export class AdminsController {
   constructor(private readonly adminsService: AdminsService) {}
 
+  @ApiTags('admin')
   @Get()
   getAllAdmins(): any {
     return this.adminsService.getAdmins();
   }
 
+  @ApiTags('admin')
   @Get(':id') // 'admins/:id
   getAdmin(@Param('id') id: string): any {
     return this.adminsService.getAdmin(id);
   }
 
+  @ApiTags('admin')
   @Post()
   createAdmin(@Body() body: any): any {
     return this.adminsService.createAdmin(body);
