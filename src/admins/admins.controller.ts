945cabfee0f1e5707a2b933ad84ec5d53ceda34d
Tristan <tres@duck.com>
2024-05-29 01:23:21 +0000

    chore: Update npm dependencies and add class-validator package

--- src/admins/admins.controller.ts
+++ src/admins/admins.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { AdminsService } from './admins.service';
 
 @Controller('admins')
 export class AdminsController {
   constructor(private readonly adminsService: AdminsService) {}
 
-  @Get('/')
+  @Get()
   getAllAdmins(): string {
     return this.adminsService.getAdmins();
+  }
+
+  @Get(':id') // 'admins/:id
+  getAdmin(@Param('id') id: string): any {
+    return this.adminsService.getAdmin(id);
+  }
+
+  @Post()
+  createAdmin(@Body() body: any): string {
+    return this.adminsService.createAdmin(body);
   }
 }
--- src/admins/admins.service.ts
+++ src/admins/admins.service.ts
@@ -1,8 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class AdminsService {
+  private admins = [
+    {
+      id: 1,
+      name: 'admin1',
+      email: 'admin@gmail.com',
+      password: 'admin123',
+    },
+  ];
+
   getAdmins(): string {
     return JSON.stringify({ message: 'admins' });
+  }
+
+  createAdmin(admin): string {
+    const id = crypto.randomUUID();
+    admin.id = id;
+    this.admins.push(admin);
+    return JSON.stringify({ message: 'create admin with id ' + id });
+  }
+
+  getAdmin(id): any {
+    const admin = this.admins.find((admin) => admin.id === id);
+
+    if (!admin) {
+      throw new NotFoundException(`Admin with id ${id} not found`);
+    }
+
+    return admin;
   }
 }
