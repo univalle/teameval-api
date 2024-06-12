440006e59dd8bbee882e8f0f9ec58536f0cebc08
Tristan <tres@duck.com>
2024-06-12 05:32:37 +0000

    refactor: Remove unused HttpExceptionFilter from main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
-import { HttpExceptionFilter } from './common/filters/http-exception.filter'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -22,7 +21,6 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document)
 
   app.enableCors()
-  app.useGlobalFilters(new HttpExceptionFilter())
 
   await app.listen(3000)
 }
